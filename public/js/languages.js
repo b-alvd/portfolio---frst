4e494dc2a8b73bb2a3fae9a72dc7ae682586457f
Basile <basilealeveque@gmail.com>
2024-02-13 12:40:06 +0100

    modifs header + start responsive for header and sections

--- public/js/languages.js
+++ public/js/languages.js
@@ -1,29 +1,32 @@
 const translations = {
     fr: {
-        header_title: 'Basile ALEVEQUE-DESSOLIN',
         header_link1: 'Accueil',
         header_link2: 'Informations',
         header_link3: 'Parcours',
         header_link4: 'Projets',
         header_link5: 'Contact',
+        
+        header_title: 'Basile ALEVEQUE-DESSOLIN',
     },
     en: {
-        header_title: 'Basile ALEVEQUE-DESSOLIN',
         header_link1: 'Home',
         header_link2: 'Informations',
         header_link3: 'Course',
         header_link4: 'Projects',
         header_link5: 'Contact',
+        
+        header_title: 'Basile ALEVEQUE-DESSOLIN',
     }
 };
   
 function setLanguage(lang) {
-    document.getElementById('header_title').innerText = translations[lang].header_title;
     document.getElementById('header_link1').innerText = translations[lang].header_link1;
     document.getElementById('header_link2').innerText = translations[lang].header_link2;
     document.getElementById('header_link3').innerText = translations[lang].header_link3;
     document.getElementById('header_link4').innerText = translations[lang].header_link4;
     document.getElementById('header_link5').innerText = translations[lang].header_link5;
+    
+    document.getElementById('header_title').innerText = translations[lang].header_title;
 }
 function changeLanguage() {
     const langSelect = document.getElementById('langSelect');
